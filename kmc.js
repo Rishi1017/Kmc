645c3948e5f624e53c9e4c894ca2d7d28bd6e40b
Rishi1017 <153922183+Rishi1017@users.noreply.github.com>
2026-05-13 00:48:04 +0800

    Add navigation functions for project pages

---
+++ kmc.js
@@ -0,0 +1,15 @@
+function goToProjects() {
+  alert("Go to Projects Page (Next Step)");
+}
+
+function goToWorkers() {
+  alert("Go to Workers Page");
+}
+
+function goToCost() {
+  alert("Go to Cost Estimator");
+}
+
+function goToPhotos() {
+  alert("Go to Site Photos");
+}
